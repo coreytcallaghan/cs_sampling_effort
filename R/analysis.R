d4d464408c9b25ad6066df68bd897242cbfb7158
Corey Callaghan <callaghan.corey.t@gmail.com>
2020-12-21 09:02:06 +0100

    more stuff from Corey playing with things

--- R/analysis.R
+++ R/analysis.R
@@ -125,3 +125,115 @@ final_results_5 <- bind_rows(lapply(c(2014:2019), function(x){predict_checklists
 
 saveRDS(final_results_5, "Results/5_km_grid_prediction_results.RDS")
 
+
+
+### A second version of the analysis
+### flipping the response and predictor variables
+# function to predict richness
+predict_completeness <- function(year, grid_size) {
+  
+  
+  dat <- readRDS(paste0("Intermediate grid level data/sampling_profile/", grid_size, "km_bcr31_", year, ".RDS"))
+  
+  preds <- read_csv(paste0("Data/predictor_data_for_grids/stats_", grid_size, "km.csv")) %>%
+    dplyr::select(-`system:index`, -`.geo`)
+  
+  # run a function for a given level of q
+  different_order <- function(q){
+    
+    filtered_dat <- dat %>%
+      dplyr::filter(Order.q==q) %>%
+      left_join(., preds, by="grid_id") %>%
+      dplyr::select(number_checklists, Estimate,
+                    heterogeneity, `tree-coverfraction`,
+                    `urban-coverfraction`, `water-permanent-coverfraction`) %>%
+      rename(tree=`tree-coverfraction`) %>%
+      rename(urban=`urban-coverfraction`) %>%
+      rename(water=`water-permanent-coverfraction`)
+    
+    # see relationship
+    filtered_dat %>%
+      cor(.) %>%
+      ggcorrplot(lab=TRUE)+
+      theme_bw()+
+      theme(axis.text=element_text(color="black"))+
+      xlab("")+
+      ylab("")+
+      theme(axis.text.x=element_text(angle=45, hjust=1))+
+      ggtitle(paste0(year, "; ", grid_size, "km; q=", q))
+    
+    # run a random forest model
+    set.seed(123)
+    samp <- sample(nrow(filtered_dat), 0.8 * nrow(filtered_dat))
+    train <- filtered_dat[samp, ]
+    test <- filtered_dat[-samp, ]
+    
+    
+    mod <- randomForest(Estimate ~ ., data = train)
+    
+    mod
+    
+    pred <- data.frame(predicted_estimate=predict(mod, newdata = test)) %>%
+      mutate(observed_estimate=test$Estimate)
+    
+    ggplot(pred, aes(x=predicted_estimate, y=observed_estimate))+
+      geom_point()+
+      #scale_x_log10()+
+      #scale_y_log10()+
+      geom_smooth(method="lm")+
+      theme_bw()+
+      theme(axis.text=element_text(color="black"))
+    
+    r2 <- pred %>%
+      lm(predicted_estimate ~ observed_estimate, data=.) %>%
+      summary() %>%
+      .$r.squared
+    
+    # write a function for different levels of checklists
+    different_checklists <- function(checklists){
+      # now create dataframe to predict on, using all grid cells
+      newdata <- preds %>%
+        dplyr::select(heterogeneity, `tree-coverfraction`,
+                      `urban-coverfraction`, `water-permanent-coverfraction`) %>%
+        rename(tree=`tree-coverfraction`) %>%
+        rename(urban=`urban-coverfraction`) %>%
+        rename(water=`water-permanent-coverfraction`) %>%
+        mutate(number_checklists=checklists)
+      
+      
+      full_prediction <- data.frame(predicted_estimate=predict(mod, newdata=newdata)) %>%
+        mutate(grid_id=preds$grid_id) %>%
+        mutate(checklists=checklists) %>%
+        mutate(q=q) %>%
+        mutate(year=year) %>%
+        mutate(grid_size=grid_size) %>%
+        mutate(model_r2=r2) %>%
+        left_join(., dat %>% 
+                    dplyr::select(grid_id, number_checklists) %>%
+                    distinct(), by="grid_id")
+      
+      return(full_prediction)
+      
+    }
+    
+    # apply this function over different levels of completeness
+    checklist_results <- bind_rows(lapply(seq(10, 1000, by=10), different_checklists))
+    
+  }
+  
+  # apply this function over different levels of q
+  results <- bind_rows(lapply(c(0, 1, 2), different_order))
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/summarizing_results.R
@@ -0,0 +1,129 @@
+# summarize results
+
+library(dplyr)
+library(ggplot2)
+library(sf)
+
+results_20km <- readRDS("Results/20_km_grid_prediction_results.RDS") %>%
+  mutate(q=paste0("q=", as.character(q)))
+
+
+ggplot(results_20km, aes(x=coverage, y=predicted_checklists, group=coverage, fill=q))+
+  geom_violin()+
+  stat_summary(fun='mean', geom='point', size=2, col='black')+
+  coord_flip()+
+  facet_wrap(q~year, ncol=6)+
+  scale_y_log10()+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  xlab("Coverage")+
+  ylab("Number of checklists")+
+  scale_x_discrete(limits=c("85%", "90%", "95%", "100%"), 
+                   breaks=c("85%", "90%", "95%", "100%"), 
+                   labels=c("85%", "90%", "95%", "100%"))+
+  scale_fill_brewer(palette="Dark2")+
+  theme(axis.text.x=element_text(size=6))
+
+ggsave("Figures/prelim_20km_fig.png", width=8.8, height=6.9, units="in")
+
+results_20km %>%
+  dplyr::select(q, year, model_r2) %>%
+  distinct() %>%
+  group_by(q) %>%
+  summarize(mean_r2=mean(model_r2))
+  
+grids_20 <- st_read("Spatial data/20_km_grids_shape/study_extent_grids_20km.geojson")
+
+plot_dat <- grids_20 %>%
+  left_join(., results_20km, by="grid_id") %>%
+  dplyr::filter(q %in% c("q=0", "q=2")) %>%
+  dplyr::filter(coverage=="95%")
+
+ggplot()+
+  geom_sf(data=plot_dat, aes(fill=log10(predicted_checklists)))+
+  facet_wrap(q~year, ncol=3)+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  scale_fill_viridis_c(name="Number of samples (log10):", breaks=c(1.8, 2.1, 2.4), labels=c(63, 126, 250))+
+  theme(legend.position="bottom")+
+  theme(axis.text=element_text(size=6))
+  
+ggsave("Figures/prelim_20km_fig_maps.png", width=8.8, height=6.9, units="in")
+
+# how many grids have met the number of checklists needed
+plot_dat2 <- grids_20 %>%
+  left_join(., results_20km, by="grid_id") %>%
+  dplyr::filter(q %in% c("q=0", "q=2")) %>%
+  dplyr::filter(coverage=="95%") %>%
+  mutate(sampled=ifelse(number_checklists>predicted_checklists, "Yes", "No"))
+
+ggplot()+
+  geom_sf(data=plot_dat2, aes(fill=sampled))+
+  facet_wrap(q~year, ncol=3)+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  scale_fill_viridis_d(name="Number of samples (log10):", breaks=c(1.8, 2.1, 2.4), labels=c(63, 126, 250))+
+  theme(legend.position="bottom")+
+  theme(axis.text=element_text(size=6))
+
+results_20km %>%
+  dplyr::select(predicted_checklists, q, coverage, grid_id, year) %>%
+  dplyr::filter(coverage=="95%") %>%
+  dplyr::filter(year==2019) %>%
+  pivot_wider(names_from="q", values_from="predicted_checklists") %>%
+  ggplot(., aes(x=`q=0`, y=`q=2`))+
+  geom_point()+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  geom_smooth(method="lm")
+  
+
+ggplot(plot_dat2, aes(x=predicted_checklists))
+
+
+
+results_5km <- readRDS("Results/5_km_grid_prediction_results.RDS") %>%
+  mutate(q=paste0("q=", as.character(q)))
+
+
+ggplot(results_5km, aes(x=coverage, y=predicted_checklists, group=coverage, fill=q))+
+  geom_violin()+
+  stat_summary(fun='mean', geom='point', size=2, col='black')+
+  coord_flip()+
+  facet_wrap(q~year, ncol=6)+
+  scale_y_log10()+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  xlab("Coverage")+
+  ylab("Number of checklists")+
+  scale_x_discrete(limits=c("85%", "90%", "95%", "100%"), 
+                   breaks=c("85%", "90%", "95%", "100%"), 
+                   labels=c("85%", "90%", "95%", "100%"))+
+  scale_fill_brewer(palette="Dark2")+
+  theme(axis.text.x=element_text(size=6))
+
+ggsave("Figures/prelim_5km_fig.png", width=8.8, height=6.9, units="in")
+
+results_5km %>%
+  dplyr::select(q, year, model_r2) %>%
+  distinct() %>%
+  group_by(q) %>%
+  summarize(mean_r2=mean(model_r2))
+
+grids_5 <- st_read("Spatial data/5_km_grids_shape/study_extent_grids_5km.geojson")
+
+plot_dat <- grids_5 %>%
+  left_join(., results_5km, by="grid_id") %>%
+  dplyr::filter(q=="q=2") %>%
+  dplyr::filter(coverage=="95%")
+
+ggplot()+
+  geom_sf(data=plot_dat, aes(fill=log10(predicted_checklists)))+
+  facet_wrap(~year, ncol=3)+
+  theme_bw()+
+  theme(axis.text=element_text(color="black"))+
+  scale_fill_viridis_c(name="Number of samples (log10):")+
+  theme(legend.position="bottom")+
+  theme(axis.text=element_text(size=6))
+
+
