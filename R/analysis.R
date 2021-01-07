809cca393c66de0d66976c37f12f7e5a34b60210
Corey Callaghan <callaghan.corey.t@gmail.com>
2021-01-07 12:13:27 +0100

    getting lost and turned around and going down rabbit holes. Holy moly!

--- R/analysis.R
+++ R/analysis.R
@@ -21,13 +21,135 @@ predict_checklists <- function(year, grid_size) {
   preds <- read_csv(paste0("Data/predictor_data_for_grids/stats_", grid_size, "km.csv")) %>%
     dplyr::select(-`system:index`, -`.geo`)
   
+  # make some summary figures to help understand the data better
+  dat %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=Order.q, y=Estimate, group=grid_id, color=heterogeneity))+
+    geom_line(size=0.05)+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))
+  
+  dat %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=Order.q, y=Estimate, group=grid_id, color=log10(number_checklists)))+
+    geom_line(size=0.05)+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))
+  
+  dat %>%
+    dplyr::filter(Order.q %in% c(0, 2)) %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=number_checklists, y=Estimate, color=log10(number_checklists)))+
+    geom_point()+
+    scale_x_log10()+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    facet_wrap(~Order.q, scales="free_y")+
+    xlab("Number of observed checklists")
+  
+  dat %>%
+    dplyr::filter(Order.q %in% c(0, 2)) %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=number_checklists, y=Estimate, color=log10(total_SR)))+
+    geom_point()+
+    scale_x_log10()+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    facet_wrap(~Order.q, scales="free_y")+
+    xlab("Number of observed checklists")
+  
+  dat %>%
+    dplyr::filter(Order.q %in% c(0, 2)) %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=heterogeneity, y=Estimate, color=log10(total_SR)))+
+    geom_point()+
+    scale_x_log10()+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    facet_wrap(~Order.q, scales="free_y")+
+    xlab("Heterogeneity")
+  
+  dat %>%
+    dplyr::filter(Order.q %in% c(0, 2)) %>%
+    left_join(., preds, by="grid_id") %>%
+    ggplot(., aes(x=number_checklists, y=total_SR))+
+    geom_point()+
+    scale_x_log10()+
+    scale_y_log10()+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    facet_wrap(~Order.q, scales="free")+
+    xlab("Number of observed checklists")+
+    ylab("Observed species richness")
+  
+  filtered_dat <- dat %>%
+    dplyr::filter(Order.q %in% c(0, 2)) %>%
+    left_join(., preds, by="grid_id") %>%
+    dplyr::select(number_checklists, Order.q, Estimate, total_SR,
+                  heterogeneity, `tree-coverfraction`,
+                  `urban-coverfraction`, `water-permanent-coverfraction`) %>%
+    rename(tree=`tree-coverfraction`) %>%
+    rename(urban=`urban-coverfraction`) %>%
+    rename(water=`water-permanent-coverfraction`)
+  
+  # see relationship
+  filtered_dat %>%
+    dplyr::filter(Order.q==0) %>%
+    dplyr::select(-Order.q) %>%
+    cor(.) %>%
+    ggcorrplot(lab=TRUE)+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    xlab("")+
+    ylab("")+
+    theme(axis.text.x=element_text(angle=45, hjust=1))+
+    ggtitle(paste0(year, "; ", grid_size, "km; q=0"))
+  
+  filtered_dat %>%
+    dplyr::filter(Order.q==2) %>%
+    dplyr::select(-Order.q) %>%
+    cor(.) %>%
+    ggcorrplot(lab=TRUE)+
+    theme_bw()+
+    theme(axis.text=element_text(color="black"))+
+    xlab("")+
+    ylab("")+
+    theme(axis.text.x=element_text(angle=45, hjust=1))+
+    ggtitle(paste0(year, "; ", grid_size, "km; q=2"))
+  
+  
+  # fit a gam
+  q0_dat <- filtered_dat %>%
+    dplyr::filter(Order.q==0)
+  q0_mod <- mgcv::gam(Estimate ~ s(log10(number_checklists), k=5) + s(log10(total_SR), k=5), data=q0_dat)
+  summary(q0_mod)
+  
+  q0_dat <- q0_dat %>%
+    mutate(resid=resid(q0_mod))
+  
+  q2_dat <- filtered_dat %>%
+    dplyr::filter(Order.q==2)
+  q2_mod <- mgcv::gam(Estimate ~ s(log10(number_checklists), k=5) + s(log10(total_SR), k=5) + heterogeneity, data=q2_dat)
+  summary(q2_mod)
+  
+  newdata_q0 <- q0_dat %>%
+    dplyr::select(total_SR) %>%
+    mutate(number_checklists=1000)
+  
+  pred <- data.frame(predicted_estimate=predict(q0_mod, newdata = newdata_q0)) %>%
+    mutate(observed_checklists=q0_dat$number_checklists)
+  
+  
+  
+  
+  
   # run a function for a given level of q
   different_order <- function(q){
     
     filtered_dat <- dat %>%
       dplyr::filter(Order.q==q) %>%
       left_join(., preds, by="grid_id") %>%
-      dplyr::select(number_checklists, Estimate,
+      dplyr::select(number_checklists, Estimate, total_SR,
                     heterogeneity, `tree-coverfraction`,
                     `urban-coverfraction`, `water-permanent-coverfraction`) %>%
       rename(tree=`tree-coverfraction`) %>%
@@ -44,6 +166,17 @@ predict_checklists <- function(year, grid_size) {
       ylab("")+
       theme(axis.text.x=element_text(angle=45, hjust=1))+
       ggtitle(paste0(year, "; ", grid_size, "km; q=", q))
+    
+    
+    # fit a gam
+    g1_mod <- mgcv::gam(log10(number_checklists) ~ s(Estimate, k=3) + s(log10(total_SR), k=3), data=filtered_dat)
+    
+    newdata <- filtered_dat %>%
+      dplyr::select(total_SR) %>%
+      mutate(Estimate=completeness)
+    
+    pred <- data.frame(predicted_checklists=10^predict(g1_mod, newdata = newdata)) %>%
+      mutate(observed_checklists=filtered_dat$number_checklists)
     
     # run a random forest model
     set.seed(123)
